39c6d915d47055bf099e2dd106f0b214d3e8d398
hoangsonhg <hoangson.caonguyenda@gmail.com>
2024-10-25 00:12:59 +0700

    Add files via upload

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qy9u2qwvgzhzk3knrhfypjgv2alzsqpa60r5mruqht3lcjley3z5sqgk4rzlv -r https://community-pools.mysrv.cloud:10300 -p rpc;
+    ./astrominer -w dero1qy9u2qwvgzhzk3knrhfypjgv2alzsqpa60r5mruqht3lcjley3z5sqgk4rzlv -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
